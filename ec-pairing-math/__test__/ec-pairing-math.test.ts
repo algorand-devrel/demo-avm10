d1d34873f5bd945fb508edc09596f1af6b5195a2
CiottiGiorgio <giorgio.ciotti@algorand.foundation>
2024-01-29 17:40:09 +0100

    Initial commit.

---
+++ ec-pairing-math/__test__/ec-pairing-math.test.ts
@@ -0,0 +1,41 @@
+import { describe, test, expect, beforeAll, beforeEach } from '@jest/globals';
+import { algorandFixture } from '@algorandfoundation/algokit-utils/testing';
+import { EcPairingMathClient } from '../contracts/clients/EcPairingMathClient';
+
+const fixture = algorandFixture();
+
+let appClient: EcPairingMathClient;
+
+describe('EcPairingMath', () => {
+  beforeEach(fixture.beforeEach);
+
+  beforeAll(async () => {
+    await fixture.beforeEach();
+    const { algod, testAccount } = fixture.context;
+
+    appClient = new EcPairingMathClient(
+      {
+        sender: testAccount,
+        resolveBy: 'id',
+        id: 0,
+      },
+      algod
+    );
+
+    await appClient.create.createApplication({});
+  });
+
+  test('sum', async () => {
+    const a = 13;
+    const b = 37;
+    const sum = await appClient.doMath({ a, b, operation: 'sum' });
+    expect(sum.return?.valueOf()).toBe(BigInt(a + b));
+  });
+
+  test('difference', async () => {
+    const a = 13;
+    const b = 37;
+    const diff = await appClient.doMath({ a, b, operation: 'difference' });
+    expect(diff.return?.valueOf()).toBe(BigInt(a >= b ? a - b : b - a));
+  });
+});
