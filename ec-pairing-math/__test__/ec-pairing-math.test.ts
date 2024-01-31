20c375f37dcc0bf2f78a75d4b5256c250e4c8a3b
CiottiGiorgio <giorgio.ciotti@algorand.foundation>
2024-01-31 15:17:57 +0100

    Wrote an example of the (partial) algorithm for BLS signature verification.

--- ec-pairing-math/__test__/ec-pairing-math.test.ts
+++ ec-pairing-math/__test__/ec-pairing-math.test.ts
@@ -25,17 +25,46 @@ describe('EcPairingMath', () => {
     await appClient.create.createApplication({});
   });
 
-  test('sum', async () => {
-    const a = 13;
-    const b = 37;
-    const sum = await appClient.doMath({ a, b, operation: 'sum' });
-    expect(sum.return?.valueOf()).toBe(BigInt(a + b));
-  });
-
-  test('difference', async () => {
-    const a = 13;
-    const b = 37;
-    const diff = await appClient.doMath({ a, b, operation: 'difference' });
-    expect(diff.return?.valueOf()).toBe(BigInt(a >= b ? a - b : b - a));
+  // The EC points used in this example were generated with the following command:
+  // python3 bls_sig_g1.py "Hello, world!" -v
+  test('verify', async () => {
+    const verified = await appClient
+      .compose()
+      .verify({
+        publicKeyPoint: Buffer.concat([
+          Buffer.from(
+            '1297cde9cb2fd3f0a1197b86dd2c20528786082632521494b907d158b84986ca7c95ac969da09439577fe6325ce298d415c06eaea978a058cfb7f752dba3e3731aeb11e91212ea7d74acc2b427f471e36d1b373ca9a5b63d1cc22b2fc476c6f5',
+            'hex'
+          ),
+          Buffer.from(
+            '0de9f9b19beb5c54955e820c2fdb31ecbcc34dbcd55bba860aa6becc5ef887454586c45ba3cb7fbe8870f3a27a8f065e05aea3c6113505c440e035b43c768eafa61dad50bf79ae6d86d6b8ef81e55338a314e31bb578fca3ad75150dcbbe517f',
+            'hex'
+          ),
+        ]),
+        clearTextPoint: Buffer.concat([
+          Buffer.from(
+            '0fcd68c4b0a514d5c1479a4b6db70e9510b42ee41afc4cd080068a6e1334c557a95e7ddf16e98f4c3e6d149266c7bd2a',
+            'hex'
+          ),
+          Buffer.from(
+            '06bb59ad79dee99f87788a29bb2d8fda0563b4c974efe23a4bd4995da7fd0e7d4d52af10fcff0ae43f60ff0b7adf1bef',
+            'hex'
+          ),
+        ]),
+        signaturePoint: Buffer.concat([
+          Buffer.from(
+            '11d2e96b168672dedea44bee7be12243e7d1ca014d2db484310e3a34993880eaea37ff65118c04224528b7ba10d5f510',
+            'hex'
+          ),
+          Buffer.from(
+            '139225f8d02ca73f5a35080a74e7ff44918c0a5e036005ddec7158d892009228489464af6e5ef0691673f152e05882e5',
+            'hex'
+          ),
+        ]),
+      })
+      .simulate({
+        extraOpcodeBudget: 320_000,
+      });
+    expect(verified.returns).toStrictEqual([true]);
   });
 });
--- ec-pairing-math/contracts/ec-pairing-math.algo.ts
+++ ec-pairing-math/contracts/ec-pairing-math.algo.ts
@@ -2,46 +2,18 @@ import { Contract } from '@algorandfoundation/tealscript';
 
 // eslint-disable-next-line no-unused-vars
 class EcPairingMath extends Contract {
-  /**
-   * Calculates the sum of two numbers
-   *
-   * @param a
-   * @param b
-   * @returns The sum of a and b
-   */
-  private getSum(a: number, b: number): number {
-    return a + b;
+  private pairingCheck(messagePoint: bytes, publicKeyPoint: bytes, signaturePoint: bytes): boolean {
+    return ecPairingCheck(
+      'BLS12_381g1',
+      messagePoint + signaturePoint,
+      publicKeyPoint +
+        // This is the inverse of the generator of G2 in the python reference implementation.
+        hex('024aa2b2f08f0a91260805272dc51051c6e47ad4fa403b02b4510b647ae3d1770bac0326a805bbefd48056c8c121bdb813e02b6052719f607dacd3a088274f65596bd0d09920b61ab5da61bbdc7f5049334cf11213945d57e5ac7d055d042b7e') +
+        hex('0d1b3cc2c7027888be51d9ef691d77bcb679afda66c73f17f9ee3837a55024f78c71363275a75d75d86bab79f74782aa13fa4d4a0ad8b1ce186ed5061789213d993923066dddaf1040bc3ff59f825c78df74f2d75467e25e0f55f8a00fa030ed')
+    );
   }
 
-  /**
-   * Calculates the difference between two numbers
-   *
-   * @param a
-   * @param b
-   * @returns The difference between a and b.
-   */
-  private getDifference(a: number, b: number): number {
-    return a >= b ? a - b : b - a;
-  }
-
-  /**
-   * A method that takes two numbers and does either addition or subtraction
-   *
-   * @param a The first number
-   * @param b The second number
-   * @param operation The operation to perform. Can be either 'sum' or 'difference'
-   *
-   * @returns The result of the operation
-   */
-  doMath(a: number, b: number, operation: string): number {
-    let result: number;
-
-    if (operation === 'sum') {
-      result = this.getSum(a, b);
-    } else if (operation === 'difference') {
-      result = this.getDifference(a, b);
-    } else throw Error('Invalid operation');
-
-    return result;
+  verify(publicKeyPoint: bytes, clearTextPoint: bytes, signaturePoint: bytes): boolean {
+    return this.pairingCheck(clearTextPoint, publicKeyPoint, signaturePoint);
   }
 }
